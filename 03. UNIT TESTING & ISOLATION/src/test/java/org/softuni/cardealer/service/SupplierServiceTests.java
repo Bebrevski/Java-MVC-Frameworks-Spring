7e25d344a19b0e884312bb6dbba5fa612fd2e9be
Bebrevski <stefan.bebrevski89@gmail.com>
2019-03-11 21:22:44 +0200

    Unit tests

--- 03. UNIT TESTING & ISOLATION/src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
+++ 03. UNIT TESTING & ISOLATION/src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
+import org.softuni.cardealer.domain.entities.Supplier;
 import org.softuni.cardealer.domain.models.service.SupplierServiceModel;
 import org.softuni.cardealer.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,9 @@ public class SupplierServiceTests {
     public void init(){
         this.modelMapper = new ModelMapper();
         this.supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+
+//        Correct way is here to seed some data in the database
     }
 
     @Test
@@ -51,5 +55,57 @@ public class SupplierServiceTests {
         toBeSaved.setName(null);
 
         supplierService.saveSupplier(toBeSaved);
+    }
+
+    @Test
+    public void supplierService_editSupplierWithCorrectValues_ReturnsCorrect(){
+        Supplier supplier = new Supplier();
+        supplier.setName("Pesho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        SupplierServiceModel toBeEdited = new SupplierServiceModel();
+        toBeEdited.setId(supplier.getId());
+        toBeEdited.setName("Gosho");
+        toBeEdited.setImporter(false);
+
+        SupplierServiceModel actual = supplierService.editSupplier(toBeEdited);
+        SupplierServiceModel expected = this.modelMapper
+                .map(this.supplierRepository.findAll().get(0), SupplierServiceModel.class);
+
+        Assert.assertEquals("Ids do not match!", expected.getId(), actual.getId());
+        Assert.assertEquals("Names do not match!", expected.getName(), actual.getName());
+        Assert.assertEquals("IsImporter do not match!", expected.isImporter(), actual.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void supplierService_editSupplierWithNullValues_ThrowsException(){
+        Supplier supplier = new Supplier();
+        supplier.setName("Pesho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        SupplierServiceModel toBeEdited = new SupplierServiceModel();
+        toBeEdited.setId(supplier.getId());
+        toBeEdited.setName(null);
+
+        supplierService.editSupplier(toBeEdited);
+    }
+
+    @Test
+    public void supplierService_deleteSupplierWithValidId_ReturnsCorrect(){
+        Supplier supplier = new Supplier();
+        supplier.setName("Pesho");
+        supplier.setImporter(true);
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        supplierService.deleteSupplier(supplier.getId());
+
+        long expected = 0;
+        long actual = this.supplierRepository.findAll().size();
+
+        Assert.assertEquals(expected, actual);
     }
 }
